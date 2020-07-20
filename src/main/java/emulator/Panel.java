49d7e8c42617845facd357f8896bf8381e115c37
Svaught598 <Svaught598@gmail.com>
2020-07-20 16:16:47 -0700

    implemented drawing methods for GUI

--- src/main/java/emulator/Panel.java
+++ src/main/java/emulator/Panel.java
@@ -19,48 +19,79 @@ class Panel extends JPanel implements ActionListener{
 
     public Panel(){
         setBackground(Color.BLACK);
-        Timer timer = new Timer(DELAY, this);
-        timer.start();
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         draw(g);
-        Toolkit.getDefaultToolkit().sync();
     }
 
+
     public void draw(Graphics g){
-        for(int i = 0; i < test; i++){
+        System.out.println("\t[INFO] Draw method called");
+        for(int i = 0; i < NUMBER_OF_PIXELS; i++){
             int posX = i % DISPLAY_WIDTH;
             int posY = (int) Math.floor(i/DISPLAY_WIDTH);
-            // if (pixelArray[i] == 1) {
-            //     g.setColor(Color.WHITE);
-            // }
-            // else {
-            //     g.setColor(Color.BLACK);
-            // }
-            g.setColor(Color.WHITE);
+
+            if (pixelArray[i] != 1) {
+                g.setColor(Color.BLACK);
+            }
+            else {
+                g.setColor(Color.WHITE);
+            }
             g.fillRect(posX*PIXEL_WIDTH, posY*PIXEL_HEIGHT, PIXEL_WIDTH, PIXEL_HEIGHT);
         }
+
     }
 
     public void actionPerformed(ActionEvent event){
-        if(test > 2048){
-            test = 1;
+        // if(test > 2048){
+        //     test = 1;
+        // }
+        // else{
+        //     test += 1;
+        // }
+        // repaint();
+    }
+
+    public void drawSprite(int posX, int posY, byte[] spriteArray){
+        // if any pixels are flipped from set to unset, set pixelFlipped = true
+        // else set pixelFlipped = false;
+        int height = spriteArray.length;
+        int index;
+        byte spriteLine;
+
+        // loop through ylines & get line for sprite
+        for (int yline = 0; yline < height; yline++){
+            spriteLine = spriteArray[yline];
+
+            // loop through xlines & get pixelarray index
+            for (int xline = 0; xline < 8; xline++){
+                index = (posX + xline) + (posY + yline)*DISPLAY_WIDTH;
+
+                // check if bit in spriteLine is unset
+                if ((spriteLine & (0x80 >> xline)) != 0){
+                    
+                    // check if bit in pixelArray is set
+                    if (pixelArray[index] == 1){
+                        pixelFlipped = true;
+                    }
+                }
+                System.out.println((spriteLine & (0x80 >> xline)) >> (8 - xline));
+                pixelArray[index] ^= 1;
+            }
         }
-        else{
-            test += 1;
-        }
+        System.out.println("\t[INFO] Sprite Painted");
         repaint();
     }
 
-    public void drawSprite(int posX, int posY, int height){
-        // if any pixels are flipped from set to unset, set pixelFlipped = true
-        // else set pixelFlipped = false;
-    }
-
     public void clearScreen(){
-        // This method should clear the screen!
+        // clears the screen!
+        for (int i = 0; i < NUMBER_OF_PIXELS; i++){
+            pixelArray[i] = 0;
+        }
+        System.out.println("\t[INFO] Screen Cleared");
+        repaint();
     }
 }
