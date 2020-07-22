7cf58c4eacbb925dff281af0d8263b5a298b7a78
Svaught598 <Svaught598@gmail.com>
2020-07-22 07:04:19 -0700

    changed look & feel

--- src/main/java/emulator/Main.java
+++ src/main/java/emulator/Main.java
@@ -11,6 +11,12 @@ public class Main{
     public static void main(String[] args){
         SwingUtilities.invokeLater(new Runnable() {
             public void run(){
+                try{
+                    UIManager.setLookAndFeel(
+                        UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
                 startGUI();
             }
         });
